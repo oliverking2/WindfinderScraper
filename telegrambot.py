992a3231f1988d24ca880f4b551296e6ec139934
Oliver <oliver@oliverking.me.uk>
2022-03-04 22:02:59 +0000

    Telegram bot will now send forecast daily at 8am

--- telegrambot.py
+++ telegrambot.py
@@ -4,11 +4,11 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 import logging
 import windfinder
 import os
+import datetime
 
 from dotenv import load_dotenv
 
 load_dotenv()
-
 TOKEN = os.getenv("TOKEN")
 chat_id = os.getenv("chat_id")
 
@@ -24,15 +24,20 @@ def windfinderBot(context: CallbackContext):
     context.bot.send_message(job.context, text=text)
 
 
-def remove_job_if_exists(name: str, context: CallbackContext):
+def remove_job(name: str, context: CallbackContext):
     current_jobs = context.job_queue.get_jobs_by_name(name)
     for job in current_jobs:
         job.schedule_removal()
 
 
+def start(update: Update, context: CallbackContext):
+    context.job_queue.run_daily(windfinderBot, datetime.time(hour=8), context=chat_id, name=str(chat_id))  # Windfinder
+
+    update.message.reply_text('Starting')
+
+
 def stop(update: Update, context: CallbackContext):
-    chat_id = update.message.chat_id
-    remove_job_if_exists(str(chat_id), context)
+    remove_job(str(chat_id), context)
 
     update.message.reply_text("Stopping")
 
@@ -43,11 +48,10 @@ def main():
 
     dispatcher = updater.dispatcher
 
+    dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("stop", stop))
 
     updater.start_polling()
-
-    job.run_repeating(windfinderBot, 10, context=chat_id, name=str(chat_id))  # Windfinder
 
     updater.idle()
 
