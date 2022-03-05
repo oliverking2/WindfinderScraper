241db450dfcc9f5d3ed7435efc4318902e66d04c
Oliver <oliver@oliverking.me.uk>
2022-03-05 01:19:06 +0000

    Facebook Post bot created and formatting of forecast adjusted

--- facebookbot.py
+++ facebookbot.py
@@ -12,10 +12,13 @@ location = "weston_southampton"
 
 forecast = windfinder.produceForecastText(location)
 
-message = "Yooo, here is the forecast for the following week and when you could take the boats out, please let me know if you plan on taking them out!\n\n" + forecast + "\n\nThis data is produced by a computer so please check the forecasts and tides before you go at sailing just to confirm!"
-link = "https://www.windfinder.com/forecast/weston_southampton"
+def postToGroup(group):
+    message = "Yooo, here is the forecast for the following week and when you could take the boats out, please let me know if you plan on taking them out!\n\n" + forecast + "\n\nThis data is produced by a computer so please check the forecasts and tides before you go at sailing just to confirm!"
+    link = "https://www.windfinder.com/forecast/weston_southampton"
 
-graph = GraphAPI(access_token=facebookAccessToken)
+    graph = GraphAPI(access_token=facebookAccessToken)
 
-graph.put_object(pageID, "feed", message=message, link=link)
-print(graph.get_connections(pageID, "feed"))
+    graph.put_object(group, "feed", message=message, link=link)
+
+postToGroup(pageID)
+
